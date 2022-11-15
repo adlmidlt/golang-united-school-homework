0ea1d7df4368357056b75955a27d2e433f4c704c
Anatolii Larionov <oiktsi8@zavodsvet.ru>
2022-11-15 11:11:54 +0400

    Решение 2-го задания

---
+++ golang-united-school-homework-2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"golang-united-school-homework-2/solution"
+)
+
+func main() {
+	fmt.Println(solution.CalcSquare(10.0, solution.SidesCircle))
+	fmt.Println(solution.CalcSquare(10.0, solution.SidesSquare))
+	fmt.Println(solution.CalcSquare(10.0, solution.SidesTriangle))
+}
+\adlmidlt\golang-united-school-homework
---
+++ golang-united-school-homework-2/solution/solution.go
@@ -0,0 +1,31 @@
+package solution
+
+import "math"
+
+// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+
+// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
+// it's like:
+// CalcSquare(10.0, SidesTriangle)
+// CalcSquare(10.0, SidesSquare)
+// CalcSquare(10.0, SidesCircle)
+
+const SidesTriangle = 3
+const SidesSquare = 4
+const SidesCircle = 0
+
+func CalcSquare(sideLen float64, sidesNum int) float64 {
+	if sidesNum == 3 {
+		return 0.5 * math.Sqrt(sideLen)
+	}
+
+	if sidesNum == 4 {
+		return math.Sqrt(sideLen)
+	}
+
+	if sidesNum == 0 {
+		return math.Pi * math.Sqrt(sideLen)
+	}
+
+	return 0.0
+}
